1a927c44c91f90cd5b93eef8900c0cd72ac4cf23
unknown <istvan.felhelyes@gmail.comm>
2020-11-19 09:50:07 +0100

    small refinements

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,36 +1,36 @@
 'use strict';
 
-const modal = document.getElementById("modalBG");
-const btn = document.getElementById("modalBtn");
-const span = document.getElementsByClassName("modal__close")[0];
-const okey = document.getElementById("modalOkey");
-const cancel = document.getElementById("modalCancel");
+const modal = document.querySelector('#modalBG');
+const btn = document.querySelector('#modalBtn');
+const span = document.querySelector('.modal__close');
+const okey = document.querySelector('#modalOkey');
+const cancel = document.querySelector('#modalCancel');
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 }
 
 // When the user clicks on Okey, close the modal and clg-Okey
 okey.onclick = function() {
-    modal.style.display = "none";
-    console.log('Okey');
+  modal.style.display = 'none';
+  console.log('Okey');
 }
 
 // When the user clicks on Cancel, close the modal and clg-Cancel
 cancel.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
     console.log('Cancel');
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 }
